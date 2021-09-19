52f2c6bc47e17be05dcb7ff700a22b487fa85a99
Kortney Foley <kortneyfoley@Kortneys-MacBook-Pro.local>
2021-09-19 17:00:49 -0300

    made a calculator that can add, subtract, multiply & divide

--- calc.py
+++ calc.py
@@ -1,1 +1,27 @@
-hello
+import operator
+
+firstNumber = 0
+secondNumber = 0 
+chosenOperator = ""
+answer = 0
+
+operatorMap = {
+    "+":operator.add,
+    "-":operator.sub,
+    "*":operator.mul,
+    "/":operator.truediv
+}
+
+firstNumber = input("Enter your first number: ")
+firstNumber = float(firstNumber)
+
+secondNumber = input("Enter second number: ")
+secondNumber = float(secondNumber)
+
+chosenOperator = input("please select operator (+, -, *, /):  ")
+
+
+answer = operatorMap[chosenOperator](firstNumber,secondNumber)
+
+print("your answer is: " , answer)
+
